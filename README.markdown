77f1271e4eaaee111691861c78b09dced554fbbd
David Rogers <david@ethos-development.com>
2012-05-02 11:44:10 -0400

    Well, Github is probably right about that README file... And I'll just use that for my "long_description".

---
+++ README.markdown
@@ -0,0 +1,9 @@
+ethos Toolkit for Google App Engine
+===================================
+
+I love me some Google App Engine. And while I do love me some Django, too, sometimes
+the full Django stack is just too much. Anymore, I prefer writing small, ReSTful
+applications using just `webapp` and `webapp2`, `Mako+HAML` templates, and `WTForms`.
+This little toolkit describes that stack nicely and provides some nice integrations
+for them as well as some decent unit testing tools for `RequestHandler` classes.
+
--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
 from setuptools import setup, find_packages
+import os.path
+
+def read(filename):
+    filepath = os.path.join(os.path.dirname(__file__), filename)
+
+    try:
+        return open(filepath).read()
+
+    except IOError:
+        return None
+
+
 setup(
-    name='GAE Toolkit',
+    name='ethos Toolkit for Google App Engine',
     version='0.1',
     packages=find_packages('src'), package_dir={'': 'src'},
-    install_requires=['decorator', 'PyHAML', 'Mako', 'WTForms'],
+    install_requires=['PyHAML', 'Mako', 'WTForms'],
 
     author='David Rogers',
     author_email='david@ethos-development.com',
-    description='A small toolkit for building GAE apps with my preferred tools.',
     ## TODO: license='???',
     url='http://github.com/al-the-x/ethos-gae-toolkit',
+    description='A small toolkit for building GAE apps with my preferred tools.',
+    long_description=read('README.markdown'),
 )
